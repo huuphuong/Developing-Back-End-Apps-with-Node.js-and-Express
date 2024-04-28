69c07933978b38d6a3ae8a9bf7dcc247ebc7c1d9
PhuongHuu <huu.phuong195@gmail.com>
2024-04-28 22:02:02 +0700

    add middleware

--- BookReviewApp/index.js
+++ BookReviewApp/index.js
@@ -1,31 +1,24 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const routes = require('./router/routes.js').publicRoute;
+const customersRoutes = require('./router/customersRoutes.js').customersRoutes;
+const requiredLogin = require('./middleware/AuthMiddleware.js');
 
 const app = express();
 
 app.use(express.json());
 
+// using session
+app.use(session({
+	secret: 'fingerprint_customer', // Change this to a long, random string
+	resave: false,
+	saveUninitialized: true
+}));
 
-// app.use("/customer/auth/*", function auth(req,res,next) {
-
-// if(req.session.authorization) {
-//     let token = req.session.authorization['accessToken']; // Access Token
-//     jwt.verify(token, "access",(err,user)=>{
-//         if(!err){
-//             req.user = user;
-//             next();
-//         }
-//         else{
-//             return res.status(403).json({message: "User not authenticated"})
-//         }
-//      });
-//  } else {
-//      return res.status(403).json({message: "User not logged in"})
-//  }
-// });
- 
+// All routes with prefix "customers" then make sure that person had login
+app.use("/customers", requiredLogin);
 
 app.use("/", routes);
-app.listen(3000, ()=>console.log("Server is running") );
+app.use("/", customersRoutes);
+
+app.listen(3000, () => console.log("Server is running") );
